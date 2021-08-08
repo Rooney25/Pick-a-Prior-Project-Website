c656b2ebbd210ac00c21cc695b1c9d1e2a82ccba
Rooney25 <87777127+Rooney25@users.noreply.github.com>
2021-08-08 12:45:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementsByTagName('button').innerHTML = location.href='https://www.linkedin.com/in/rooney-salas-32640b218/';
+}
---
+++ main2.js
@@ -0,0 +1,3 @@
+function otherFunction() {
+    document.getElementsByTagName('button').innerHTML = location.href='https://www.nps.k12.nj.us/SCI/';
+}
